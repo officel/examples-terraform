62a5b7282611e5ae7bd81917676ea664007ba7b6
Nishimura Yoshitaka <officel@users.noreply.github.com>
2021-10-24 01:57:35 +0900

    feat: time provider resources (#15)
    時間関係で遊んでみた。
    これを使うと再作成の選択が自動化できる気がする。

---
+++ provider/time/main.tf
@@ -0,0 +1,45 @@
+# 時間を管理
+
+resource "time_rotating" "this" {
+  # リソースをローテーションさせるための間隔
+  rotation_minutes = 3
+}
+
+resource "time_static" "this" {
+  # 引数なしで現在時刻
+  # トリガーでローテーションの間隔で作り直し
+  triggers = {
+    rotating = time_rotating.this.rfc3339
+  }
+}
+
+resource "time_sleep" "this" {
+  # 作成時に delay する
+  create_duration = "5s"
+
+  triggers = {
+    rotating = time_static.this.rfc3339
+  }
+}
+
+resource "time_offset" "this" {
+  offset_hours = 1
+
+  depends_on = [
+    time_sleep.this
+  ]
+
+  triggers = {
+    rotating = time_static.this.rfc3339
+  }
+}
+
+
+output "time_provider" {
+  value = <<-EOT
+    time_static:   ${format("%v is current datetime", time_static.this.rfc3339)}
+    time_sleep:    ${format("%v is delay %v", time_sleep.this.id, time_sleep.this.create_duration)}
+    time_rotating: ${format("%v is next rotating datetime", time_rotating.this.rotation_rfc3339)}
+    time_offset:   ${format("%v is offset 1 hour + %v from current datetime", time_offset.this.rfc3339, time_sleep.this.create_duration)}
+  EOT
+}
